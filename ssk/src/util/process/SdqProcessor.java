a64c9435774de5fb01d1045ba17d0d0d140490b9
jioniy <jw010208@gmail.com>
2023-01-02 13:37:13 +0900

    [FIX] #5 SDQ 점수 계산 오류 수정

--- ssk/src/util/process/SdqProcessor.java
+++ ssk/src/util/process/SdqProcessor.java
@@ -10,16 +10,17 @@ public class SdqProcessor {
 		
 		for(int i=0;i<sdqReplyList.size();i++) {
 			int QNUM = sdqReplyList.get(i).getSdqQuestionId()%10;
+			int score = sdqReplyList.get(i).getSdqReplyContent()-1;//enum 값은 1,2,3,4 이나, 점수는 0,1,2,3
 			if(QNUM==1 ||QNUM == 2) {//사회 지향 행동
-				scoreList[0]+=sdqReplyList.get(i).getSdqReplyContent();
+				scoreList[0]+=score;
 			}else if(QNUM==3||QNUM==4) {//과잉행동
-				scoreList[1]+=sdqReplyList.get(i).getSdqReplyContent();
+				scoreList[1]+=score;
 			}else if(QNUM==5||QNUM==6) {//정서 증상
-				scoreList[2]+=sdqReplyList.get(i).getSdqReplyContent();
+				scoreList[2]+=score;
 			}else if(QNUM==7||QNUM==8) {//품행문제
-				scoreList[3]+=sdqReplyList.get(i).getSdqReplyContent();
+				scoreList[3]+=score;
 			}else if(QNUM==9||QNUM==10) {//또래문제
-				scoreList[4]+=sdqReplyList.get(i).getSdqReplyContent();
+				scoreList[4]+=score;
 			}
 		}
 		
