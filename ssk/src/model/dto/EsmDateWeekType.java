eb994815f7dfe2108f4038b0cc6065b8e7484949
jioniy <jw010208@gmail.com>
2023-01-25 20:49:06 +0900

    [FIX] #8 시간별 그래프에서 일별그래프로 전환 시 오류 수정

--- ssk/src/model/dto/EsmDateWeekType.java
+++ ssk/src/model/dto/EsmDateWeekType.java
@@ -6,6 +6,7 @@ import java.sql.Date;
  * 정서 반복 기록
  * 주간 첫 기록 일자(date)와 해당 주를 나타내는 문자열(weekStr)을 나타낸 자료형
  * esmResult.jsp 페이지에서 드롭다운에 나타내기 위해 이용하는 data type
+ * weekStr값만 일치하면 같은 값으로 취급
  */
 public class EsmDateWeekType {
 	private Date date;
@@ -35,6 +36,6 @@ public class EsmDateWeekType {
 	
 	@Override
 	public boolean equals(Object o){
-		return ( date.equals(((EsmDateWeekType)o).date) && weekStr.equals(((EsmDateWeekType)o).weekStr));
+		return (weekStr.equals(((EsmDateWeekType)o).weekStr));
 	}
 }
