b1590a923a84dfe18f79d28c150933af7681325f
jioniy <jw010208@gmail.com>
2023-02-14 22:34:21 +0900

    [REFAC] #1 Dynamic Web Project to Maven Project

---
+++ ssk/src/controller/expert/exportChildResultExcel.java
@@ -0,0 +1,42 @@
+package controller.expert;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+ 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ * 아동 결과 엑셀 파일 export
+ */
+@WebServlet("/exportChildResultExcel")
+public class exportChildResultExcel extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public exportChildResultExcel() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
