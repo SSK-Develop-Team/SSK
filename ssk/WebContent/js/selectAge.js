13b35228e215e5a598f03a4893da956ce0438bd9
w1thAug <sys4who@gmail.com>
2023-03-09 11:04:03 +0900

    [REFAC] LangTest Main Modal

---
+++ ssk/WebContent/js/selectAge.js
@@ -0,0 +1,15 @@
+
+
+function ageCheckClose(){
+	document.getElementById('ageCheck').style.display='none';
+}
+
+function ageSelect(){
+	document.getElementById('ageCheck').style.display='none';
+	document.getElementById('ageSelect').style.display='block';
+}
+
+function ageSelectClose(){
+	document.getElementById('ageSelect').style.display='none';
+}
+
