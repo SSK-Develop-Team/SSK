6253bdf03a53ad7c15d6c477688289ffeb1d8e57
jioniy <jw010208@gmail.com>
2023-12-27 22:17:26 +0900

    [ADD] #72 Age group changed

--- ssk/src/model/sevice/AgeGroupService.java
+++ ssk/src/model/sevice/AgeGroupService.java
@@ -10,9 +10,10 @@ public class AgeGroupService {
     /**
      * 언어 발달 검사 - 선택 가능한 연령 그룸 반환 함수
      *  : curAge를 기준으로 12개월 전부터 6개월 후까지
+     *  2023.10.05수정 => 모든 연령에 대한 검사가 가능하도록 수정함
      * */
     public static ArrayList<AgeGroup> getSelectableAgeGroupList(Connection con, AgeGroup currAgeGroup){
-
+/*
         int startAge = currAgeGroup.getStartAge() - 12;
 
         int endAge = currAgeGroup.getEndAge() + 6;
@@ -21,7 +22,11 @@ public class AgeGroupService {
             startAge = 36;
             endAge = 120;
         }
+*/
+    	int startAge = 36;
 
+        int endAge = 120;
+        
         ArrayList<AgeGroup> ageGroupList = AgeGroupDAO.getAgeGroupListInRange(con, startAge, endAge);
 
         return ageGroupList;
